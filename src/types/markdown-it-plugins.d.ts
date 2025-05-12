da9d81fcba46bf222b9e9349b095005bef87323c
蹲家 <13433126742@163.com>
2025-05-12 11:01:50 +0800

    clean unused code

--- src/types/markdown-it-plugins.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'markdown-it';
-declare module 'markdown-it-anchor';
-declare module 'markdown-it-emoji';
-declare module 'markdown-it-task-lists';
-declare module 'markdown-it-highlightjs';
-declare module 'markdown-it-toc-done-right';

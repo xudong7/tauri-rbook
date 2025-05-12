fa374038d43313ad6ea8daf65982d9c43afc41b1
蹲家 <13433126742@163.com>
2025-05-12 10:58:44 +0800

    back add read epub file

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
+use epub::doc::EpubDoc;
 use serde::{Deserialize, Serialize};
-use std::fs;
 use std::path::Path;
+use std::{fs, vec};
 mod tray;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -10,6 +11,13 @@ struct ReadResult {
     filename: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct EpubFile {
+    content: Vec<u8>,
+    mime: String,
+}
+
+// Read a markdown file and return its content and filename
 #[tauri::command]
 fn read_markdown_file(path: &str) -> Result<ReadResult, String> {
     // Read the file
@@ -25,6 +33,25 @@ fn read_markdown_file(path: &str) -> Result<ReadResult, String> {
     Ok(ReadResult { content, filename })
 }
 
+// read epub file
+#[tauri::command]
+fn read_epub_file(path: &str) -> Result<Vec<EpubFile>, String> {
+    let doc = EpubDoc::new(path).unwrap();
+    let mut returned: Vec<EpubFile> = vec![];
+    let mut doc_clone = EpubDoc::new(path).unwrap();
+    doc.resources.keys().for_each(|k| {
+        if let Some(content) = doc_clone.get_resource(k) {
+            let (content, mime) = content;
+            let part = EpubFile {
+                content: content.to_vec(),
+                mime: mime.to_string(),
+            };
+            returned.push(part);
+        }
+    });
+    Ok(returned)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -36,7 +63,7 @@ pub fn run() {
         })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
-        .invoke_handler(tauri::generate_handler![read_markdown_file])
+        .invoke_handler(tauri::generate_handler![read_markdown_file, read_epub_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
